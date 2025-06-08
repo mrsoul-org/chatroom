b7223b24dc7ab8374cc5e940ffec93f0118971d0
vscharan <charanv369@gmail.com>
2025-06-08 07:01:19 +0530

    updated the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ IMAGE_NAME="vootlasaicharan/chatroom-application:latest"
 # Stop and remove existing container if it exists
 if docker ps -a | grep -q "$CONTAINER_NAME"; then
     echo "Stopping and removing existing container..."
-    docker stop "$CONTAINER_NAME" && docker rm "$CONTAINER_NAME" && docker rmi "$IMAGE_NAME"
+    docker stop "$CONTAINER_NAME" && docker rm "$CONTAINER_NAME" && docker rmi $(docker images -q) || true
     echo "Container stopped and removed."
 fi
 
